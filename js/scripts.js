13dd7628b4fae0697683489ca28bcf429eca9acb
Jarod Sjogren <jss29576@uga.edu>
2018-10-01 17:03:03 -0400

    New website look

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$('div').blast({
+    delimiter: 'Word'
+});
+
+$('.blast').mouseenter(function () {
+    $(this).velocity("transition.bounceIn", { complete: function() { $(this).velocity("stop", true)}});
+
+});
